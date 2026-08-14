8f92feef5fd0e733e23cc72d05d1fab185ec8512
sh2624 <sh2624@daum.net>
2026-08-14 19:44:38 +0900

    Feat: 하단바 및 마이페이지 구현

--- src/styles/AppLayout.jsx
+++ src/styles/AppLayout.jsx
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
+import BottomNav from '../components/common/BottomNav';
 
-const AppLayout = ({ children }) => {
+const AppLayout = () => {
   return (
     <Background>
-      <MobileContainer>{children}</MobileContainer>
+      <MobileContainer>
+        <ContentScrollArea>
+          <Outlet />
+        </ContentScrollArea>
+      </MobileContainer>
+      <BottomNav />
     </Background>
   );
 };
@@ -21,12 +28,21 @@ const Background = styled.div`
 
 // 실제 모바일 화면 영역
 const MobileContainer = styled.div`
+  position: relative;
   width: 100%;
   max-width: 430px;
   height: 100dvh;
   background-color: #ffffff;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05);
+  overflow: hidden;
+`;
+
+// 스크롤 영역
+const ContentScrollArea = styled.div`
+  width: 100%;
+  height: 100%;
   overflow-y: auto;
+  padding-bottom: 100px;
 
   &::-webkit-scrollbar {
     display: none;
